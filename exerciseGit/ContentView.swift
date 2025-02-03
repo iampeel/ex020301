ba69c45d64f106933bd5ed0a4e1aa3e74c10008a
Moonpil Cheon <iampeel@naver.com>
2025-02-03 21:17:09 +0900

    Initial Commit

---
+++ exerciseGit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  exerciseGit
+//
+//  Created by 천문필 on 2/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ exerciseGit/exerciseGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  exerciseGitApp.swift
+//  exerciseGit
+//
+//  Created by 천문필 on 2/3/25.
+//
+
+import SwiftUI
+
+@main
+struct exerciseGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
